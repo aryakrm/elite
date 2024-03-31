77b514cbe5ada640099678e3b9314759e9f6fab2
Arya Karimi <devaryakrm@gmail.com>
2024-03-31 22:33:00 +0300

    images problem solved

--- src/app/components/Sec4/Sec4.jsx
+++ src/app/components/Sec4/Sec4.jsx
@@ -1,4 +1,6 @@
 import "./Sec4.scss"
+import Image from "next/image"
+import process1 from "../../../../public/static/images/process1.png"
 
 function Sec4() {
   return (
@@ -6,7 +8,14 @@ function Sec4() {
       <h1>Our Process</h1>
       <h2>World Class Service</h2>
       <div className="cards">
-        <div></div>
+        <div>
+        <Image
+      src={process1}
+      width={350}
+      height={350}
+      alt="process1"
+    /> 
+        </div>
         <div></div>
         <div></div>
       </div>
